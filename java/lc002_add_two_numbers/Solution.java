2203261cfabf9b8e57caf6c8355537ef3f3b976f
duncan_zhou <duncan.tree.zhou@gmail.com>
2019-10-02 10:42:40 +0800

    init

--- java/lc002_add_two_numbers/Solution.java
+++ java/lc002_add_two_numbers/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     // init l2
     ListNode l2 = new ListNode(5);
     l2.next = new ListNode(6);
-    l2.next = new ListNode(4);
+    l2.next.next = new ListNode(4);
 
     ListNode result = addTwoNumbers(l1,l2);
     String resultStr = getResultStr(result);
