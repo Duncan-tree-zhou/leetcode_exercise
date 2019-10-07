c2e8c969a44c00534335c1af18c5b6000ba1f4f2
duncan_zhou <duncan.tree.zhou@gmail.com>
2019-10-07 09:08:22 +0800

    lc0004 solution2 寻找两个有序数组的中位数

---
+++ java/lc0004_median_of_two_sorted_arrays/Solution2.java
@@ -0,0 +1,56 @@
+package lc0004_median_of_two_sorted_arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution2 {
+
+  public static void main(String[] ags){
+    int[] nums1 = new int[]{1, 4, 7, 9, 15, 18, 18};
+    int[] nums2 = new int[]{2, 4, 6, 8, 10, 15, 18, 22};
+    double result = findMedianSortedArrays(nums1, nums2);
+    String resultStr = result + "";
+    String expectedStr = "8.5";
+    System.out.println(String.format("result is : %s",resultStr));
+    System.out.println(String.format("expected is : %s", expectedStr));
+  }
+
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int n1 = nums1.length;
+    int n2 = nums2.length;
+
+    if (n1 == 0) {
+      if (n2 == 0) {
+        return 0.0;
+      } else {
+          return findMedianInOneSortedArray(nums2);
+      }
+    } else if (n2 == 0) {
+      return findMedianInOneSortedArray(nums1);
+    }
+
+    int n = n1 + n2;
+    if (n%2 != 0) {
+      return findKInSortedArrays(nums1, 0, nums2, 0, n/2);
+    } else {
+      return (findKInSortedArrays(nums1, 0, nums2, 0, n/2) + findKInSortedArrays(nums1, 0, nums2, 0, n/2+1)) / 2
+    }
+  }
+
+  public static double findKInSortedArrays(int[] nums1, int n1start, int[] nums2, int n2start, int k) {
+
+    return -1;
+  }
+
+  public static double findMedianInOneSortedArray(int[] nums) {
+    if (nums.length%2 == 0) {
+      return (nums[nums.length/2 - 1] + nums[nums.length/2]) / 2;
+    } else {
+      return nums[nums.length/2];
+    }
+  }
+
+}
